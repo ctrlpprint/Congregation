872372a7294d3e3ab752e3402de804b2760a1a1d
David <ctrlpprintblog@gmail.com>
2012-09-30 23:22:10 +1300

    Add SignalR. Add a home page.
    Use Resharper to generate View. Manually remove the Layout reference so
    default can be used.

---
+++ app/Congregation.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Congregation.Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		// GET: /Home/
+		public ActionResult Index() {
+			return View();
+		}
+
+		// GET: /Home/Details/5
+		//public ActionResult Details(int id) {
+		//    return View();
+		//}
+
+		// GET: /Home/Create
+		//public ActionResult Create() {
+		//    return View();
+		//}
+
+		// POST: /Home/Create
+		//[HttpPost]
+		//public ActionResult Create(FormCollection collection) {
+		//    try {
+		//        // TODO: Add insert logic here
+
+		//        return RedirectToAction("Index");
+		//    }
+		//    catch {
+		//        return View();
+		//    }
+		//}
+
+	}
+}
