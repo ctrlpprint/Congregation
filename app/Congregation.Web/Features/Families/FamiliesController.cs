b52beb0d751ca577b066467c24c128d9cc63bba3
David <ctrlpprintblog@gmail.com>
2012-10-15 10:45:07 +1300

    Create a Family View page

---
+++ app/Congregation.Web/Features/Families/FamiliesController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Congregation.Application.Data;
+using Congregation.Core.Models.Families;
+
+namespace Congregation.Web.Features.Families
+{
+    public class FamiliesController : CommonController
+    {
+    	private readonly IRepository repository;
+
+    	public FamiliesController(IRepository repository) {
+			this.repository = repository;
+		}
+
+    	public ActionResult View(int id) {
+    		var family = repository.Get<Family>(id);
+
+            return View(family);
+        }
+
+    }
+}
